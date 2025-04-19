2ef8417be0ea81c9b16166df0b7dced45fab3b9a
Rafael A. Ortega <raortega8906@gmail.com>
2025-04-19 22:00:31 +0200

    add Tasks model and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
