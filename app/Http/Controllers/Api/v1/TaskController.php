415767e480f4c5164af30e57e9ca8e2db90fa654
Rafael A. Ortega <raortega8906@gmail.com>
2025-04-21 02:24:20 +0200

    add store and update request for task, add store function for task

--- app/Http/Controllers/Api/v1/TaskController.php
+++ app/Http/Controllers/Api/v1/TaskController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\StoreTaskRequest;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         $tasks = Task::all();
 
@@ -19,12 +21,43 @@ class TaskController extends Controller
             ]);
         }
 
+
         $data = [
             'message' => 'Tasks retrieved successfully',
             'status' => 200,
             'data' => $tasks
         ];
-        
+
         return response()->json($data);
     }
+
+    public function store(StoreTaskRequest $request): JsonResponse
+    {
+        try {
+
+            $validated = $request->validated();
+            $validated['status'] = 'pending';
+
+            $task = Task::create($validated);
+
+            $data = [
+                'message' => 'Task created successfully',
+                'status' => 201,
+                'data' => $task
+            ];
+
+            return response()->json($data);
+
+        } 
+        catch (\Exception $e) {
+
+            return response()->json([
+                'message' => 'Failed to create task',
+                'status' => 500,
+                'error' => $e->getMessage()
+            ]);
+
+        }
+    }
+
 }
