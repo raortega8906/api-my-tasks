e59409d5338eec20bcd026ca3c3375e77c0c0de6
Rafael A. Ortega <raortega8906@gmail.com>
2025-05-07 23:38:17 +0200

    change title of welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Backend API Task</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
